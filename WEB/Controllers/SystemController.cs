0ed5092e65f835e882032c4da1611c399c5caa7b
BuildTools <unconfigured@null.spigotmc.org>
2018-12-08 14:23:05 +0800

    12

--- WEB/Controllers/SystemController.cs
+++ WEB/Controllers/SystemController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WEB.Models;
 
 namespace WEB.Controllers
 {
     public class SystemController : Controller
     {
-        // GET: System
+        [HttpGet]
         public ActionResult Index()
-        {
+        { 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login()
+        {
+
+            return RedirectToAction("index");
         }
     }
 }
---
+++ WEB/Models/paramModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB.Models
+{
+    public class paramModel<T>
+    {
+        public int status { get; set; }
+        public string msg { get; set; }
+        public List<T> data { get; set; }
+    }
+}
